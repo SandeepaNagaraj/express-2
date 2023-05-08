cf791436f271994837c5d13e7f560511bfc53c23
SandeepaNagaraj <sandeepanagaraj4@gmail.com>
2023-05-08 11:16:14 +0530

    Initial is done to save the file structure

--- services.js
+++ services.js
@@ -1,6 +1,37 @@
+
 const express =require('express')
-const servicesRouter=express.Router()
-servicesRouter.get('/',(request,response)=>{
-    response.send("This is Services page")
+const userRouter = express.Router()
+const listofUsers =[
+    {
+        'id':1,
+        'name':'web development'
+    },
+    {
+        'id':2,
+        'name':'cyber security'
+    },
+    {
+        'id':3,
+        'name':'Data Science'
+    },
+    {
+        'id':4,
+        'name':'Machine Learning'
+    }
+]
+userRouter.get('/',(request,response)=>{
+response.send('This is list of users')
 })
-module.exports=servicesRouter
+userRouter.get('/:id([0-9]{1})',(request,response)=>{
+    const pageId=Number(request.params.id)
+
+    const userId =listofUsers.find((user)=> user.id ===pageId)
+    if(!userId){
+        response.send('page not found')
+    }
+    else{
+        response.json(userId.name)
+    }
+
+})
+module.exports = userRouter
